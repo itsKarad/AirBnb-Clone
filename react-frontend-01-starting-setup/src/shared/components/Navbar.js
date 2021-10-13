7332a24a23c511e9400b12482e98795a256b6e18
Karad <akarad@ee.iitr.ac.in>
2021-10-13 21:23:07 +0530

    Removed all warnings

--- react-frontend-01-starting-setup/src/shared/components/Navbar.js
+++ react-frontend-01-starting-setup/src/shared/components/Navbar.js
@@ -5,7 +5,9 @@ const Navbar = (props) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand">Your Places App</a>
+                <div className="navbar-brand">
+                    Your Places App
+                </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
